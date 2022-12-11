fdad1723295347947547d138ff7c7efd0b7ba6f9
lumerocode <luismelendez0894@gmail.com>
2022-12-11 14:44:36 -0500

    cambios para la sgunda preentrega

--- Js/javascript.js
+++ Js/javascript.js
@@ -1,14 +1,18 @@
-const carrito = document.querySelector("#carrito");
-const template = document.querySelector("#template");
+const cart = document.querySelector("#cart");
 const footer = document.querySelector("#footer");
 const templateFooter = document.querySelector("#templateFooter");
+const template = document.querySelector("#template");
 const fragment = document.createDocumentFragment();
-const agregar = document.querySelectorAll(".card button");
 
-const carritoObjeto = {};
+let cartArray = [];
+
 
 document.addEventListener("click", (e) => {
-    
+
+    if (e.target.matches(".card .btn-add")) {
+        agregarCarrito(e);
+    }
+
     if (e.target.matches(".list-group-item .btn-success")) {
         btnAumentar(e);
     }
@@ -18,48 +22,52 @@ document.addEventListener("click", (e) => {
     }
 });
 
-//Agregar Carrito
 const agregarCarrito = (e) => {
 
-    const producto = {
+    const product = {
         titulo: e.target.dataset.product,
         id: e.target.dataset.product,
         cantidad: 1,
         precio: parseInt(e.target.dataset.price),
+        imagen: e.target.dataset.img,
     };
 
-    if(carritoObjeto.hasOwnProperty(producto.titulo)) {
-        producto.cantidad = carritoObjeto[producto.titulo].cantidad + 1;
-    }
+     // Searching the index
+     const index = cartArray.findIndex((item) => item.id === product.id);
 
-    carritoObjeto[producto.titulo] = producto
+     // si no existe empujamos el nuevo elemento
+     if (index === -1) {
+        cartArray.push(product);
+     } else {
+         // en caso contrario aumentamos su cantidad
+         cartArray[index].cantidad++;
+     }
 
     pintarCarrito();
 
 };
 
-agregar.forEach((btn) => btn.addEventListener("click", agregarCarrito));
-
 const pintarCarrito = () => {
-    carrito.textContent = "";
+    cart.textContent = "";
 
-    Object.values(carritoObjeto).forEach(item => {
+    cartArray.forEach(item => {
         const clone = template.content.cloneNode(true);
         clone.querySelector(".lead").textContent = item.titulo;
         clone.querySelector(".badge").textContent = item.cantidad;
-        clone.querySelector("div .lead span").textContent = item.precio * item.cantidad;;
+        clone.querySelector("div .lead span").textContent = item.precio * item.cantidad;
+        clone.querySelector("div .img-single-product").src=item.imagen;
         clone.querySelector(".btn-success").dataset.id = item.id;
         clone.querySelector(".btn-danger").dataset.id = item.id;
-
         fragment.appendChild(clone);
     });
 
-    carrito.appendChild(fragment);
+    cart.appendChild(fragment);
+    pintarFooter();
 };
 
 const btnAumentar = (e) => {
-    console.log(e.target.dataset.id);
-    Object.values(carritoObjeto).forEach((item) => {
+    // console.log(e.target.dataset.id);
+    cartArray.forEach((item) => {
         if (item.id === e.target.dataset.id) {
             item.cantidad++;
         }
@@ -70,12 +78,10 @@ const btnAumentar = (e) => {
 
 const btnDisminuir = (e) => {
     // console.log(e.target.dataset.id);
-    Object.values(carritoObjeto).forEach((item) => {
-        // console.log(item);
+    cartArray.forEach((item) => {
         if (item.id === e.target.dataset.id) {
             if (item.cantidad > 0) {
                 item.cantidad--;
-                // console.log(item);
                 if (item.cantidad === 0) return;
                 return item;
             }
@@ -85,3 +91,23 @@ const btnDisminuir = (e) => {
     });
     pintarCarrito();
 };
+
+const pintarFooter = () => {
+    footer.textContent = "";
+
+    const total = cartArray.reduce(
+        (acc, current) => acc + current.precio * current.cantidad,
+        0
+    );
+
+    const clone = templateFooter.content.cloneNode(true);
+    clone.querySelector("p span").textContent = total;
+
+    footer.appendChild(clone);
+};
+
+function finalizarCompra() {
+
+    window.confirm("Graciias, pronto actualizaremos más productos a la venta")
+
+}
